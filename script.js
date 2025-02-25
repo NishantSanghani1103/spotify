f3a3e664646f6e386f9cd5d9257ed6424a99c9dd
NishantSanghani1103 <nishantsanghani09@gmail.com>
2025-02-25 11:54:22 +0530

    navigation close

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let micon = document.querySelector(".micon");
 let nav = document.querySelector(".header-mid-right");
+let closeIcon = document.querySelector(".closeIcon");
 console.log(nav)
 micon.addEventListener("click", () => {
-    nav.classList.toggle("showNavMenu");
-    micon.classList.toggle("fa-close");
+    nav.classList.add("showNavMenu");
+    // micon.classList.toggle("fa-close");
 
+})
+closeIcon.addEventListener("click", () => {
+    nav.classList.remove("showNavMenu");
 })
